58a4c3148dd9858433a5dcf7b31b5fbedcc25ee0
HYONSU <admin@hyonsu.com>
2024-12-19 20:38:18 +0900

    개발자인 경우 항상 오류 표시

--- main.js
+++ main.js
@@ -395,7 +395,7 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, _) => {
     console.error(err);
-    if(debug) res.status(500).send(err.toString());
+    if(debug || req.permissions?.includes('developer')) res.status(500).send(err.toString());
     else res.status(500).send('서버 오류');
 });
 
