6096d3622989cad9347fe2255b61a6d1bedc1493
HYONSU <admin@hyonsu.com>
2024-12-24 13:56:55 +0900

    역링크 로그 개수 추가

--- routes/admin.js
+++ routes/admin.js
@@ -181,7 +181,7 @@ app.get('/admin/config/tools/:tool', middleware.permission('developer'), middlew
                 document: document.uuid
             }).sort({ rev: -1 });
             if(!rev?.content) {
-                console.log(`skipping ${document.uuid} because no content`);
+                console.log(`skipping ${document.uuid} because no content, ${total - documents.length}/${total}`);
                 continue;
             }
 
