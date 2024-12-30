90f8e3b8e2412b9d9054318a406184b3a1b66960
HYONSU <admin@hyonsu.com>
2024-12-30 15:54:55 +0900

    빈 문서 생성 허용

--- routes/document.js
+++ routes/document.js
@@ -692,7 +692,7 @@ app.post('/edit/?*', middleware.parseDocumentName, async (req, res) => {
     if(content.startsWith('#redirect ')) content = content.split('\n')[0];
     if(content.startsWith('#redirect 문서:')) content = content.replace('#redirect 문서:', '#redirect ');
 
-    if((rev?.content ?? '') === content) return res.status(400).send('문서 내용이 같습니다.');
+    if(rev?.content === content) return res.status(400).send('문서 내용이 같습니다.');
 
     // TODO: automerge
     const isCreate = rev?.content == null;
