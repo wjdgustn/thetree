626023abf8306894dd5d717ab413549eb8b3e862
HYONSU <admin@hyonsu.com>
2025-02-14 15:19:06 +0900

    토론 목록 페이지에서 스레드 삭제 시 페이지 새로고침

--- routes/discuss.js
+++ routes/discuss.js
@@ -716,7 +716,9 @@ app.post('/admin/thread/:url/delete', middleware.permission('delete_thread'), as
     });
     SocketIO.of('/thread').to(thread.uuid).emit('updateThread', { deleted: true });
 
-    res.status(204).end();
+    const referer = new URL(req.get('referer'));
+    if(referer.pathname.startsWith('/discuss/')) res.reload();
+    else res.status(204).end();
 });
 
 app.post('/admin/thread/:url/:id/:action', middleware.permission('hide_thread_comment'), async (req, res) => {
