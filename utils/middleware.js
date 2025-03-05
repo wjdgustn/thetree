4ee81f59a12628a6eb82d16ba9b68b6c152e481d
HYONSU <admin@hyonsu.com>
2025-03-05 18:47:39 +0900

    isLogin 미들웨어에 referer 사용하지 않기 close #20

--- utils/middleware.js
+++ utils/middleware.js
@@ -5,7 +5,7 @@ const {validationResult} = require('express-validator');
 module.exports = {
     isLogin(req, res, next) {
         if(!req.isAuthenticated() || req.user?.type !== UserTypes.Account)
-            return res.redirect(`/member/login?redirect=${encodeURIComponent(req.get('Referrer') || req.originalUrl)}`);
+            return res.redirect(`/member/login?redirect=${encodeURIComponent(req.originalUrl)}`);
         next();
     },
     isLogout(req, res, next) {
