520701718898c8f9971ab114a89bbe7684c8aa1a
정현수 <admin@hyonsu.com>
2025-04-24 11:10:00 +0900

    로그아웃 시 리다이렉트 사용

--- routes/member.js
+++ routes/member.js
@@ -541,7 +541,7 @@ app.get('/member/logout', middleware.isLogin, async (req, res) => {
         req.session.fullReload = true;
         delete req.session.contributor;
         res.clearCookie('honoka');
-        res.reload();
+        res.redirect(req.get('Referer') || '/');
     });
 });
 
