5896ac43bb203cdb777772fe208add74e7b31767
HYONSU <admin@hyonsu.com>
2025-04-27 12:41:52 +0900

    계정 만들기 비활성화

--- utils/types.js
+++ utils/types.js
@@ -152,20 +152,28 @@ module.exports = {
             message: ''
         },
         {
-            name: '문서 생성 비활성화',
-            methodField: 'POST',
-            type: 'js',
-            condition: `!req.permissions.includes('admin') && req.url.startsWith('/edit/') && req.body.baseuuid === 'create'`,
-            messageType: 'plaintext',
-            message: '새 문서 생성이 비활성화되어 있습니다.'
-        },
-        {
             name: '읽기 전용 모드',
             methodField: 'ALL',
             type: 'js',
-            condition: `['/edit/','/move/','/delete/','/member/login','/member/logout'].some(a => req.url.startsWith(a)) || req.method !== 'GET'`,
+            condition: `['/edit/','/move/','/delete/','/member/login','/member/logout'].some(a => url.startsWith(a)) || req.method !== 'GET'`,
             messageType: 'flexible',
             message: '위키가 읽기 전용 모드입니다.'
+        },
+        {
+            name: '계정 만들기 비활성화',
+            methodField: 'ALL',
+            type: 'js',
+            condition: `url.split('/')[1] === 'member' && url.split('/')[2] === 'signup' && !url.split('/')[3]`,
+            messageType: 'flexible',
+            message: '계정 만들기가 비활성화되어 있습니다.'
+        },
+        {
+            name: '문서 생성 비활성화',
+            methodField: 'POST',
+            type: 'js',
+            condition: `!req.permissions.includes('admin') && url.startsWith('/edit/') && req.body.baseuuid === 'create'`,
+            messageType: 'plaintext',
+            message: '새 문서 생성이 비활성화되어 있습니다.'
         }
     ]
 }
