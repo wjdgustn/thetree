95b90f8283b4674075d7a32e7118321f30ff1049
HYONSU <admin@hyonsu.com>
2025-06-26 23:27:02 +0900

    목차 생성 순서 미루기

--- utils/newNamumark/toHtml.js
+++ utils/newNamumark/toHtml.js
@@ -74,34 +74,6 @@ const topToHtml = async (parsed, options = {}) => {
     const commentPrefix = commentId ? `tc${commentId}-` : '';
 
     if(parsed.data && !options.skipInit) {
-        {
-            let html = '<div class="wiki-macro-toc">';
-            let indentLevel = 0;
-            for(let heading of parsed.data.headings) {
-                const prevIndentLevel = indentLevel;
-                indentLevel = heading.actualLevel;
-
-                const indentDiff = Math.abs(indentLevel - prevIndentLevel);
-
-                if(indentLevel !== prevIndentLevel)
-                    for(let i = 0; i < indentDiff; i++)
-                        html += indentLevel > prevIndentLevel ? '<div class="toc-indent">' : '</div>';
-
-                html += `<span class="toc-item"><a href="#${commentPrefix}s-${heading.numText}">${heading.numText}</a>. ${await toHtml(heading.linkText)}</span>`;
-
-                Store.heading.list.push({
-                    level: heading.level,
-                    num: heading.numText,
-                    title: await toHtml(heading.pureText),
-                    anchor: `s-${heading.numText}`
-                });
-            }
-            for(let i = 0; i < indentLevel + 1; i++)
-                html += '</div>';
-
-            Store.heading.html = html;
-        }
-
         const includeParams = [];
         for(let [docName, params] of Object.entries(parsed.data.includeParams)) {
             const parsedName = mainUtils.parseDocumentName(docName);
@@ -250,6 +222,34 @@ const topToHtml = async (parsed, options = {}) => {
             }
         }
         await parsedDocFinder(includeDocs);
+
+        {
+            let html = '<div class="wiki-macro-toc">';
+            let indentLevel = 0;
+            for(let heading of parsed.data.headings) {
+                const prevIndentLevel = indentLevel;
+                indentLevel = heading.actualLevel;
+
+                const indentDiff = Math.abs(indentLevel - prevIndentLevel);
+
+                if(indentLevel !== prevIndentLevel)
+                    for(let i = 0; i < indentDiff; i++)
+                        html += indentLevel > prevIndentLevel ? '<div class="toc-indent">' : '</div>';
+
+                html += `<span class="toc-item"><a href="#${commentPrefix}s-${heading.numText}">${heading.numText}</a>. ${await toHtml(heading.linkText)}</span>`;
+
+                Store.heading.list.push({
+                    level: heading.level,
+                    num: heading.numText,
+                    title: await toHtml(heading.pureText),
+                    anchor: `s-${heading.numText}`
+                });
+            }
+            for(let i = 0; i < indentLevel + 1; i++)
+                html += '</div>';
+
+            Store.heading.html = html;
+        }
 
         Store.categories = parsed.data.categories;
         for(let obj of Store.categories) {
