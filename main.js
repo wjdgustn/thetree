838aeb5750cddddb59027495028c689bc75668c7
HYONSU <admin@hyonsu.com>
2025-07-13 22:09:17 +0900

    /internal IP uuid 생성 예외 적용

--- main.js
+++ main.js
@@ -713,7 +713,9 @@ app.use(async (req, res, next) => {
             ip: req.ip
         }).lean();
 
-        if(!req.session.ipUser && req.method === 'POST' && !['/member/', '/preview/'].some(a => req.url.startsWith(a))) {
+        if(!req.session.ipUser
+            && req.method === 'POST'
+            && !['/member/', '/preview/'].some(a => req.url.startsWith(a) || req.url.startsWith('/internal' + a))) {
             const newUser = new User({
                 ip: req.ip,
                 type: UserTypes.IP
