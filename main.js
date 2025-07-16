dba28650c05856853dc4e0694f0e8e18d545985f
hyonsu <admin@hyonsu.com>
2025-07-16 16:15:40 +0900

    프론트엔드 config cspNonce 지원

--- main.js
+++ main.js
@@ -916,7 +916,7 @@ app.use(async (req, res, next) => {
         return {
             ...(userConfigHash !== configHash ? {
                 configHash,
-                config: configJSON
+                config: Object.fromEntries(Object.entries(configJSON).map(([k, v]) => [k, v.replaceAll('{cspNonce}', res.locals.cspNonce)]))
             } : {}),
             ...(userSessionHash !== sessionHash ? {
                 sessionHash,
