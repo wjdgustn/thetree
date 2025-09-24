518a9f7d324d724f9752800a21876803a829abad
HYONSU <admin@hyonsu.com>
2025-09-24 17:36:44 +0900

    minThreads 강제 설정 제거

--- utils/newNamumark/toHtml.js
+++ utils/newNamumark/toHtml.js
@@ -12,8 +12,7 @@ const worker = new Piscina({
     workerData: {
         config,
         macroPluginPaths: global.plugins.macros ?? []
-    },
-    minThreads: 30
+    }
 });
 
 module.exports = async (...params) => {
