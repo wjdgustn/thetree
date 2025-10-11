60efc7494f287fe0070e2824ddc41b0c408c1173
HYONSU <admin@hyonsu.com>
2025-10-12 00:01:58 +0900

    __proto__ 접근 제한

--- utils/newNamumark/utils/jsGlobalRemover.js
+++ utils/newNamumark/utils/jsGlobalRemover.js
@@ -20,13 +20,18 @@ let tempObj = Object;
 for(let key of [...Object.getOwnPropertyNames(globalThis).filter(a => !laterProcess.includes(a)), ...laterProcess]) {
     const item = globalThis[key];
     if(typeof item !== 'function' || key === 'log') continue;
-    if(['String', 'Array', 'Object'].includes(key)) for(let protoKey of [...tempObj.getOwnPropertyNames(item.prototype)]) {
-        // if(['caller', 'callee', 'arguments'].includes(protoKey)) continue;
-        if(whitelistedFunctions[key]?.includes(protoKey)) continue;
+    if(['String', 'Array', 'Object', 'Number', 'BigInt'].includes(key)) {
+        for(let protoKey of [...tempObj.getOwnPropertyNames(item.prototype)]) {
+            // if(['caller', 'callee', 'arguments'].includes(protoKey)) continue;
+            if(whitelistedFunctions[key]?.includes(protoKey)) continue;
 
-        const proto = item.prototype[protoKey];
-        if(typeof proto !== 'function') continue;
-        tempObj.defineProperty(item.prototype, protoKey, {
+            const proto = item.prototype[protoKey];
+            if(typeof proto !== 'function') continue;
+            tempObj.defineProperty(item.prototype, protoKey, {
+                value: undefined
+            });
+        }
+        tempObj.defineProperty(item.prototype, '__proto__', {
             value: undefined
         });
     }
