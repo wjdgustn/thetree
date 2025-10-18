6df707503f15316f5c9d9837ebf95346cd44a08e
HYONSU <admin@hyonsu.com>
2025-10-19 00:40:27 +0900

    S3_FORCE_PATH_STYLE env

--- main.js
+++ main.js
@@ -88,6 +88,7 @@ require('dotenv').config();
 global.S3 = new aws.S3Client({
     region: process.env.S3_REGION || 'auto',
     endpoint: process.env.S3_ENDPOINT,
+    forcePathStyle: process.env.S3_FORCE_PATH_STYLE === 'true',
     credentials: {
         accessKeyId: process.env.S3_ACCESS_KEY_ID,
         secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
