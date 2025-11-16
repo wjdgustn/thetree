7ba41a5f42a8bbc21ac48c8110e94adb64744025
HYONSU <admin@hyonsu.com>
2025-11-16 23:01:31 +0900

    지원 영상 확장자 추가

--- routes/special.js
+++ routes/special.js
@@ -358,7 +358,10 @@ app.post('/Upload', (req, res, next) => {
             'image/bmp',
             'image/svg+xml',
             'image/ico',
-            'video/mp4'
+            'video/mp4',
+            'video/mkv',
+            'video/mov',
+            'video/avi'
         ].includes(file.mimetype)) return res.status(400).send(
             '올바르지 않은 파일입니다.'
             + (req.permissions.includes('developer') ? ` (${file.mimetype})` : '')
