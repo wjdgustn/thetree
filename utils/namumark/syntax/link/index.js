5138aaf21995eaf4d5034180a7a097d3bf8168b6
hyonsu <admin@hyonsu.com>
2026-02-19 02:25:55 +0900

    링크 파싱 이름공간 구분에 parseDocumentName 사용

--- utils/namumark/syntax/link/index.js
+++ utils/namumark/syntax/link/index.js
@@ -31,12 +31,13 @@ module.exports = async (obj, options = {}) => {
         isImage = true;
     }
 
-    if(link.startsWith('분류:') && !thread)
+    if(mainUtils.parseDocumentName(link).namespace === '분류' && !thread)
         return;
 
     if(link.startsWith(':')) {
         const slicedLink = link.slice(1);
-        const isSpecialLink = slicedLink.startsWith('파일:') || slicedLink.startsWith('분류:');
+        const parsedSlicedLink = mainUtils.parseDocumentName(slicedLink);
+        const isSpecialLink = parsedSlicedLink.namespace === '파일' || parsedSlicedLink.namespace === '분류';
 
         if(config.namespaces.some(a => slicedLink.startsWith(a + ':'))) {
             link = slicedLink;
@@ -76,7 +77,7 @@ module.exports = async (obj, options = {}) => {
         link = parsedLink.href;
         title = link;
 
-        if(imageDocNames.length && !obj.parsedText.some(a => a.type !== 'link' || !a.link.startsWith('파일:'))) {
+        if(imageDocNames.length && !obj.parsedText.some(a => a.type !== 'link' || mainUtils.parseDocumentName(a.link).namespace !== '파일')) {
             let passedCount = 0;
             for(let docName of imageDocNames) {
                 let linkRules = config.external_link_icons?.[docName];
@@ -128,7 +129,7 @@ module.exports = async (obj, options = {}) => {
             if(hash) hasHash = true;
             title = link;
 
-            if(link.startsWith('문서:')) link = link.slice(3);
+            if(mainUtils.parseDocumentName(link).namespace === '문서') link = link.slice(3);
             if(link.includes('../')) link = `/w?doc=${encodeURIComponent(link)}${hash ? `#${hash}` : ''}`;
             else link = `/w/${globalUtils.encodeSpecialChars(link)}${hash ? `#${hash}` : ''}`;
 
