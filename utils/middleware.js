5d69f90ebfe3b7cb6cda93a2838cc63b560166da
hyonsu <admin@hyonsu.com>
2026-02-22 23:20:20 +0900

    middleware 오류 번역

--- utils/middleware.js
+++ utils/middleware.js
@@ -14,11 +14,11 @@ module.exports = {
         next();
     },
     permission: perm => (req, res, next) => {
-        if(!req.permissions.includes(perm)) return res.error('권한이 부족합니다.', 403);
+        if(!req.permissions.includes(perm)) return res.error(req.t('errors.missing_permission'), 403);
         next();
     },
     referer: pathname => (req, res, next) => {
-        const error = () => res.error('잘못된 요청입니다.');
+        const error = () => res.error(req.t('errors.invalid_request'));
 
         try {
             const referer = new URL(req.get('Referer'));
@@ -35,8 +35,8 @@ module.exports = {
     },
     parseDocumentName: (req, res, next) => {
         const name = req.params.document?.join('/') || req.query.doc;
-        if(!name) return res.error('문서 이름이 없습니다.', 404);
-        if(name.length > 255) return res.error('문서 이름이 올바르지 않습니다.', 400);
+        if(!name) return res.error(req.t('errors.missing_document_name'), 404);
+        if(name.length > 255) return res.error(req.t('errors.invalid_document_name'), 400);
         req.document = utils.parseDocumentName(name);
         next();
     },
